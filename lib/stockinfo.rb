e445e6dfda525d1a56482287a654fd779859afd1
KTAmoguis <ktamoguis@gmail.com>
2018-05-17 10:07:52 +0800

    preliminary code for class Stockinfo

--- lib/stockinfo.rb
+++ lib/stockinfo.rb
@@ -5,3 +5,28 @@ require_relative "../lib/command_line_interface.rb"
 module Stockinfo
   # Your code goes here...
 end
+
+class Stockinfo
+
+  attr_accessor :symbol, :name, :price, :change, :low, :high, :previousclose
+
+  @@all = []
+
+  def initialize(stock_hash)
+    @symbol = stock_hash[:symbo]
+    @name = stock_hash[:name]
+    @price = stock_hash[:price]
+    @change = stock_hash[:change]
+    @low = stock_hash[:low]
+    @high = stock_hash[:high]
+    @previousclose = stock_hash[:previousclose]
+    @@ll << self
+  end
+
+  def self.create_from_collection(stock_array)
+    stock_array.each do |stock|
+      Stockinfo.new(stock)
+    end
+  end
+
+end
