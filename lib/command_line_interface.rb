7e31d46c2664d238ea92ad15bebcf2e2f85eda34
KTAmoguis <ktamoguis@gmail.com>
2018-05-17 09:20:14 +0800

    initial flow of CLI

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -2,6 +2,7 @@
 #require_relative "../lib/student.rb"
 #require 'nokogiri'
 #require 'colorize'
+require 'pry'
 
 class CommandLineInterface
 
@@ -15,8 +16,40 @@ class CommandLineInterface
     puts "1. Display all stocks"
     puts "2. Pick stock"
     puts "3. Exit"
+    input = gets.strip
+    binding.pry
+
+    case input.to_i
+    when 1
+      display_all_stocks
+    when 2
+      pick_stock
+    else
+    end
   end
 
-  
+  def second_menu
+    puts "Type 'Exit' or press '1' to return to main menu"
+    input = gets.strip
+
+    if input.to_i == 1
+      main_menu
+    elsif input != "Exit"
+      second_menu
+    end
+
+  end
+
+  def display_all_stocks
+    puts "display all stocks"
+    second_menu
+  end
+
+  def pick_stock
+    puts "pick stock"
+    second_menu
+  end
+
+
 
 end
