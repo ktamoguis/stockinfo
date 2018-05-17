85e4f9a09effc71ab97e84947855f1847cdb6a3f
KTAmoguis <ktamoguis@gmail.com>
2018-05-17 14:34:55 +0800

    defined Scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,19 @@
+require_relative "../lib/command_line_interface.rb"
+
 require 'open-uri'
 require 'pry'
 require 'nokogiri'
+
+class Scraper
+
+  def self.scrape_page(url)
+    webpage = Nokogiri::HTML(open(url))
+    stocks = []
+    #each element in table = webpage.css("tbody tr")[1]
+    binding.pry
+
+
+    stocks
+  end
+
+end
