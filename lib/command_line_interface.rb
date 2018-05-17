b687346e53e7445b883141158669ba6b2ca177d6
KTAmoguis <ktamoguis@gmail.com>
2018-05-17 08:43:15 +0800

    initial code

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,11 +1,22 @@
-require_relative "../lib/scraper.rb"
-require_relative "../lib/student.rb"
-require 'nokogiri'
-require 'colorize'
+#require_relative "../lib/scraper.rb"
+#require_relative "../lib/student.rb"
+#require 'nokogiri'
+#require 'colorize'
 
 class CommandLineInterface
 
   def run
+    main_menu
   end
+
+  def main_menu
+    puts "Welcome to Stock Info (Dow 30):"
+    puts "Please select from the menu below:"
+    puts "1. Display all stocks"
+    puts "2. Pick stock"
+    puts "3. Exit"
+  end
+
+  
 
 end
--- lib/stockinfo.rb
+++ lib/stockinfo.rb
@@ -1,7 +1,7 @@
-require "stockinfo/version"
+#require "stockinfo/version"
+
+require_relative "../lib/command_line_interface.rb"
 
 module Stockinfo
   # Your code goes here...
 end
-
-puts "hello"
